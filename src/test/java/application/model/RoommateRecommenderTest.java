e3fe9e9ba262029c63ff1d1082ebfe237c966cd3
gengh <genghenry11@gmail.com>
2025-10-20 21:51:10 -0400

    Initial Commit for Recommender files.

---
+++ src/test/java/application/model/RoommateRecommenderTest.java
@@ -0,0 +1,13 @@
+package application.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * The Test class for recommending roommates.
+ */
+class RoommateRecommenderTest {
+
+
+
+
+}
