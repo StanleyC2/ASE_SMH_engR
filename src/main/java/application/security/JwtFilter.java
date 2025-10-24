7379b8451961a9d5ebafd1d6d0d68564fdab4229
gengh <genghenry11@gmail.com>
2025-10-23 20:13:01 -0400

    Added logging for API entrypoints.

--- src/main/java/application/security/JwtFilter.java
+++ src/main/java/application/security/JwtFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.logging.Logger;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -17,6 +18,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 public class JwtFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
+    private final Logger logger = Logger.getLogger(this.getClass().getName());
 
     /**
      * Filter logic to validate JWT token from Authorization header.
@@ -31,6 +33,9 @@ public class JwtFilter extends OncePerRequestFilter {
     protected void doFilterInternal(
             HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+
+        logger.info(String.format("Logged New API Request: %s %s\n", request.getMethod(),
+                request.getRequestURL()));
 
         final String authHeader = request.getHeader("Authorization");
 
